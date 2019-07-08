9c02bdec0f1f4ef05e12247f544b61f679dd5ba3
Grace Lee <igracelee1@gmail.com>
2019-07-08 14:45:00 -0400

    language selection

---
+++ translate/src/components/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+class App extends React.Component {
+    state = { language: 'english' };
+
+    onLanguageChange = language => {
+        this.setState({ language: language });
+    }
+
+    render() {
+        return (
+            <div className="ui container">
+                <div>
+                    Select a language:
+                    <i className="flag us" onClick={() => this.onLanguageChange('english')}/>
+                    <i className="flag kr" onClick={() => this.onLanguageChange('korean')}/>
+                </div>
+                {this.state.language}
+            </div>
+        );
+    }
+};
+
+export default App;
